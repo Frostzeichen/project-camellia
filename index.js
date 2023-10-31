51c010b3e6d8d3174e618ad9621cc9c0597588c4
Frostzeichen <cruztjomar@gmail.com>
2023-10-31 20:39:27 +0800

    Update index to serve ejs

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 // PRELOADING
 import "dotenv/config";
+import ejs from "ejs";
 import express from "express";
 const app = express();
-
-// import pug from "pug";
-// app.set("views", "/views"); // might want to move these to their own controller.
-// app.set("view engine", "pug");
 
 // CONSTANTS
 const port = process.env.SERVER_PORT;
 
+// MIDDLEWARES
+app.set("view engine", "ejs");
+
+// ROUTES
+let titles = [
+  "123",
+  "456",
+  "789"
+]
+app.get("/", (req, res) => {
+  res.render("index", {titles: titles}); // Automatically sets the "views" folder as home folder for some reason. Might be better to use dirname?
+})
+
+// SERVER
 const appName = "Project 茶花 || Camellia Article Writer";
 const version = "version 0.0.1";
 app.listen(port, (error) => {
@@ -18,5 +29,5 @@ app.listen(port, (error) => {
   } else {
     console.log(appName + " " + version);
     console.log("Listening! @ port " + port);
-  };
+  }
 });
