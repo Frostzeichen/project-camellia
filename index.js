238da46c72635290a07eef3d3fd7655503fdde18
Frostzeichen <cruztjomar@gmail.com>
2023-10-31 21:09:49 +0800

    Add better-sounding titles

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 // PRELOADING
 import "dotenv/config";
-import ejs from "ejs";
+import "ejs";
 import express from "express";
 const app = express();
 
 // CONSTANTS
+const appName = process.env.APP_NAME;
+const version = process.env.APP_VERSION;
 const port = process.env.SERVER_PORT;
 
 // MIDDLEWARES
 app.set("view engine", "ejs");
 
 // ROUTES
-let titles = [
-  "123",
-  "456",
-  "789"
-]
+let titles = [ // Data will come from cache which comes from database.
+  "Ang Adventures ni Mang Juan",
+  "The Pug",
+  "Natanso sa Mobile Legends"
+];
+
 app.get("/", (req, res) => {
-  res.render("index", {titles: titles}); // Automatically sets the "views" folder as home folder for some reason. Might be better to use dirname?
-})
+  res.render("index", {
+    titles:     titles,
+    ejsTitle:   appName,
+    ejsVersion: version
+  }); // Automatically sets the "views" folder as home folder for some reason. Might be better to use dirname?
+});
 
 // SERVER
-const appName = "Project 茶花 || Camellia Article Writer";
-const version = "version 0.0.1";
 app.listen(port, (error) => {
   if(error){
     console.error(error);
